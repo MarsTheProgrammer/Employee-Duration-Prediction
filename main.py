ebc114f02f0f9cc7fa871415eaef1fe6b604894e
MarsTheProgrammer <marstheprogrammer@gmail.com>
2021-12-08 08:03:39 -0600

    updated all values to match those that will be added into the jupyter array

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# education, joiningYear, city, paymentTier, age, gender,
+#   everBenched, experienceInCurrentDomain -> leaveornot
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("profile.html")
+
+@app.route('/inputdata', methods=["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+        first_name = request.form.get('fname')
+        degree = request.form.get('degree')
+        year_joined = request.form.get('year')
+        city = request.form.get('city')
+        salary = request.form.get('salary')
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        benched = request.form.get('benched')
+        experience = request.form.get('exp')
+
+        return "Name: " + first_name + \
+               " Degree: " + degree + \
+               " Joined: " + year_joined + \
+               " City: " + city + \
+               " Salary: " + salary + \
+               " Age: " + age + \
+               " Gender: " + gender + \
+               " Benched: " + benched + \
+               " Experience: " + experience
+
+        return render_template("input.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
